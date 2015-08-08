c42935c4e0a5ecb64f6518e8bf46bdd4c0add1b6
Eduardo Ottaviani <Edu@edu.local>
2015-08-08 19:51:12 -0300

    New version of sticky with reference option

---
+++ sticky/sample/assets/js/apps/home.js
@@ -0,0 +1,14 @@
+define([
+
+	'jails',
+	'comps/sticky/sticky'
+
+], function( jails, sticky ){
+
+	return jails.app('home', function( html, data ){
+
+		this.init = function(){
+		};
+
+	});
+});
--- sticky/sticky.js
+++ sticky/sticky.js
@@ -4,28 +4,70 @@ define([
 
 ], function( jails ){
 
-	return jails.component('sticky', function(html, anno){
+	return jails.component('sticky', function( html, anno ){
 
-		var
-			cp = this, win = $(window),
-			clss, top, target;
+		var cp = this, win, clss, top, target, bottom, reference, pin;
 
 		this.init = function(){
 
+			set();
+			pin();
+
+			win.on( 'scroll',  pin );
+		};
+
+		function set(){
+
+			win = $(window);
 			top = anno.top || html.data('sticky-top') || 0;
 			clss = anno.fixed || html.data('sticky-css') || 'fixed';
+			bottom = anno.bottom || html.data('sticky-bottom') || 'bottom';
+
 			target = anno.target || html.data('sticky-target');
 			target = target? $(target) :html;
 
-			win.on('scroll', pin);
-		};
+			reference = anno.reference || html.data('sticky-reference');
+			reference = reference? $( reference ) :null;
 
-		function pin(){
+			pin = reference? pin_reference : pin_single;
+		}
 
-			if( win.scrollTop() >= top )
+		function pin_single(){
+
+			if( win.scrollTop() >= top ){
 				target.addClass( clss );
-			else
+				cp.emit('topstart');
+			}
+			else{
 				target.removeClass( clss );
+				cp.emit('topend');
+			}
+		}
+
+		function pin_reference(){
+
+			var wt = win.scrollTop(),
+			offset = reference.offset().top;
+
+			//Top case
+			if( wt >= offset ){
+				target.addClass( clss );
+				cp.emit('topstart');
+			}
+			else{
+				target.removeClass( clss );
+				cp.emit('topend');
+			}
+
+			//Bottom case
+			if( wt >= reference.height() + offset - target.height() ){
+				target.addClass( bottom );
+				cp.emit('bottomin');
+			}
+			else{
+				target.removeClass( bottom );
+				cp.emit('bottomout');
+			}
 		}
 
 	});
