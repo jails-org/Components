7ad0edb14169c1ad261e297e6d37bf2eb2b93407
Eduardo Ottaviani <Edu@ottaviani.local>
2015-08-22 17:58:42 -0300

    Adding autopost component to the project

---
+++ autopost/autopost.js
@@ -0,0 +1,26 @@
+define([
+
+	'jails'
+
+], function( jails ){
+
+	return jails.component('autopost', function( html, anno ){
+
+		var cp = this, target, ev;
+
+		this.init = function(){
+
+			target = anno.target || html.data('autopost-target');
+			ev = anno.event || html.data('autopost-event') || 'change';
+
+			html.on( ev, target, submit );
+		};
+
+		function submit(){
+
+			cp.emit('post', this);
+			$( this.form ).submit();
+		}
+
+	});
+});
