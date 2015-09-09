42fc2d90960dd994e164281dd28acc29047124c0
Eduardo Ottaviani <Edu@ottaviani.local>
2015-09-09 19:00:16 -0300

    Adding if, not, each to the component itself, not only for the children

--- view/view.js
+++ view/view.js
@@ -70,7 +70,7 @@ define([
 
 		var html = el.html(), ch = $('<div />'), aux = $('<div />'), text, s_aux;
 
-		aux.append( html );
+		aux.append( el.clone() );
 
 		aux.find('[data-if]').each(function(){
 			var it = $(this), v = it.data('if');
