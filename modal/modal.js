d6ca014e5218cb56fa591767b7d56302c98f61e9
Eduardo Ottaviani <Edu@MacBook-Pro.local>
2016-08-06 03:14:53 -0300

    Adding modal and router-view components

---
+++ modal/modal.js
@@ -0,0 +1,38 @@
+define([
+
+	'jails'
+
+], function( jails ){
+
+	return jails('modal', function( component, element, anno ){
+
+		var cssopen, cssclose;
+
+		component.init = function(){
+
+			cssopen = anno.open || element.getAttribute('data-litemodal-open') || 'open';
+			cssclose = anno.close || element.getAttribute('data-litemodal-close') || '.close';
+
+			component.on('click', cssclose, component.close);
+			component.on('click', outside);
+		};
+
+		component.open = function(){
+			element.className = element.className.replace(cssopen, '') + ' ' + cssopen;
+			jails.events.on(document, 'keyup', esc);
+		};
+
+		component.close = function(){
+			element.className = element.className.replace(cssopen, '');
+			jails.events.off(document, 'keyup', esc);
+		};
+
+		function outside(e){
+			if(e.target == element) cp.close();
+		}
+
+		function esc(e){
+			if(e.which == 27) cp.close();
+		}
+	});
+});
